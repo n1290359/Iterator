b6560fb5949aff5e5d1d5e4b2fd6f9ebd552c42f
n1290359 <75464437+n1290359@users.noreply.github.com>
2021-07-01 13:52:48 +0900

    Add files via upload

---
+++ NoFactoryMethod/src/Date.java
@@ -0,0 +1,17 @@
+
+public class Date {
+	public static String today() {
+		return "2021/07/01";
+	}
+}
+class Counter{
+	private static int tvNum = 100;
+	private static int radioNum = 76;
+	
+	public static int getTvNumber() {
+		return tvNum++;
+	}
+	public static int getRadioNumber() {
+		return radioNum++;
+	}
+}
